aa6acc2231eedf0ec7851a24c415706f47160804
Tuyen Dang <tuyendq@gmail.com>
2018-07-27 13:55:26 +0700

    add index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<? php
+$welcome = "PHP with .NET on Azure";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>PHP on .NET on Azure</title>
+<meta charset="utf-8">
+</head>
+<body>
+<h1><?php $welcome ?></h2>
+</body>
+</html>
